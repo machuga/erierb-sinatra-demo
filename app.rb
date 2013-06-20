5f80e7de75a3d721dd0521bf44426147c5398cd6
Matthew Machuga <machuga@gmail.com>
2013-06-20 08:39:44 -0400

    Sinatra templates with slim

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra/base'
+require 'slim'
 
 class Erierb < Sinatra::Base
   get '/' do
     "Hello, World!"
+  end
+
+  get '/about' do
+    "Hello, World!"
+  end
+
+  get '/page' do
+    @name = "Ruby"
+    slim :page
   end
 end

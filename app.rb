aa2e6dedb9396fd249abde515515310568c827c0
Matthew Machuga <machuga@gmail.com>
2013-06-20 09:13:20 -0400

    Simple resource

--- app.rb
+++ app.rb
@@ -1,6 +1,32 @@
 require 'sinatra/base'
 require 'slim'
 
+class User
+  attr_accessor :name, :email, :role
+
+  def initialize(params)
+    puts params.inspect
+    @name = params[:name]
+    @email = params[:email]
+    @role = params[:role]
+  end
+end
+
+class UserRepository
+  @@users = []
+
+  def initialize
+  end
+
+  def self.all
+    @@users
+  end
+
+  def self.create(user)
+    @@users << user
+  end
+end
+
 class Erierb < Sinatra::Base
   get '/' do
     "Hello, World!"
@@ -13,5 +39,20 @@ class Erierb < Sinatra::Base
   get '/page' do
     @name = "Ruby"
     slim :page
+  end
+
+  get '/users' do
+    @users = UserRepository.all
+    slim :users
+  end
+
+  get '/users/new' do
+    slim :users_new
+  end
+
+  post '/users' do
+    @user = User.new(params[:user])
+    UserRepository.create @user
+    redirect '/users'
   end
 end
